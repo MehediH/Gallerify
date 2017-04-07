6b2db1f3f98b56b197ffe67ce0930fcc51cc6298
Mehedi Hassan <mehedi.dip@outlook.com>
2017-04-07 16:52:43 +0100

    Update gallerify.php

--- gallerify.php
+++ gallerify.php
@@ -9,7 +9,7 @@
  * Author URI: http://twitter.com/mehedih_
  * Text Domain: gallerify
  * License: GPL2
- * Copyright 2015  Mehedi  (email : mehedi.dip@outlook.com)
+ * Copyright 2015  Mehedi 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as 
  * published by the Free Software Foundation.
